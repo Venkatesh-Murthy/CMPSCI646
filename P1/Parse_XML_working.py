416ceb1dc448ae4c6466888d2e6eaf179e7de3dc
Venkatesh Narasimha Murthy <venk@whyalla.cs.umass.edu>
2014-09-28 16:08:13 -0400

    updated

--- P1/Parse_XML_working.py
+++ P1/Parse_XML_working.py
@@ -8,10 +8,13 @@ class DocHandler( xml.sax.ContentHandler ):
    def __init__(self,count,book_count,uniq_count,page_count,inv_index):
       self.CurrentData = ""
       self.CurrentPage=""
+      self.OldPage=-1;
       self.line=""
       self.region=""
       self.section=""
       self.page=""
+      self.page_tmp=""
+      self.pag_cont=""
       self.count=count;
       self.page_count=page_count;
       self.uniq_count=uniq_count;
@@ -41,6 +44,8 @@ class DocHandler( xml.sax.ContentHandler ):
       if(tag=='page'):
 		self.CurrentPage=attributes['id'];
                 self.page_count=self.page_count+1;
+		#self.pag_cont=self.page_tmp;
+		self.page_tmp=""
       
    def stp_word_chk(self,word):
 #	if(word=="the"):
@@ -50,8 +55,47 @@ class DocHandler( xml.sax.ContentHandler ):
 	else:
 		self.valid=1;
    # Call when an elements ends
-   #def endElement(self, tag):
-    #  if self.CurrentData == "region":
+   def endElement(self, tag):
+      #pdb.set_trace()
+      #self.pag_cont=self.page_tmp;
+      #self.page_tmp="";
+      if (tag == 'page'):
+	self.pag_cont=self.page_tmp;                                                                                                                               
+        self.page_tmp="";
+	if(self.pag_cont!=""):    
+#	 pdb.set_trace()                                                                                                                                                                                                                           
+         self.line=self.pag_cont;                                                                                                                                                                                                                           
+         self.pag_cont=""                                                                                                                                                                                                                                   
+         self.special_characters(self.line);                                                                                                                                                                                                                
+         word=self.line.split(' ')   
+	 special_words=['powerful', 'strong', 'butter', 'salt'];                                                                                                                                                                                                                       
+        # pdb.set_trace()                                                                                                                                                                                                                                   
+         for i in range(len(word)):                                                                                                                                                                                                                         
+             self.valid=1;#self.stp_word_chk(word[i]);                                                                                                                                                                                                      
+             if (self.valid==1 and word[i]!=""):                                                                                                                                                                                                            
+#               print word[i]                                                                                                                                                                                                                               
+                self.count=self.count+1;                                                                                                                                                                                                                    
+                if word[i] in self.inv_index:                                                                                                                                                                                                               
+                   if self.book_count in self.inv_index[word[i]]:                                                                                                                                                                                           
+                      if self.CurrentPage in self.inv_index[word[i]][self.book_count]:                                                                                                                                                                      
+                        self.inv_index[word[i]][self.book_count][self.CurrentPage] += 1;                                                                                                                                                                    
+                        self.inv_index[word[i]]['total']+=1;                                                                                                                                                                                                
+                      else:                                                                                                                                                                                                                                 
+                        self.inv_index[word[i]][self.book_count][self.CurrentPage] =1;                                                                                                                                                                      
+                        self.inv_index[word[i]]['total']+=1;                                                                                                                                                                                                
+        #               self.uniq_count=self.uniq_count+1;                                                                                                                                                                                                  
+                   else:                                                                                                                                                                                                                                    
+                        self.inv_index[word[i]][self.book_count]={}                                                                                                                                                                                         
+                        self.inv_index[word[i]]['total']+=1;                                                                                                                                                                                                
+                        self.inv_index[word[i]][self.book_count][self.CurrentPage]=1;                                                                                                                                                                       
+        #               self.uniq_count=self.uniq_count+1;                                                                                                                                                                                                  
+                else:                                                                                                                                                                                                                                       
+                        self.inv_index[word[i]]={};                                                                                                                                                                                                         
+                        self.inv_index[word[i]][self.book_count]={}                                                                                                                                                                                         
+                        self.inv_index[word[i]]['total']=1;                                                                                                                                                                                                 
+                        self.inv_index[word[i]][self.book_count][self.CurrentPage]=1;                                                                                                                                                                       
+                        self.uniq_count=self.uniq_count+1; 
+	
      #    print "Region:", self.region
 #      elif self.CurrentData == "section":	
  #        print "Section:", self.section
@@ -98,17 +142,22 @@ class DocHandler( xml.sax.ContentHandler ):
          self.page = content
 	 word=self.page.split(' ')
          self.count=self.count+len(word)
-      elif self.CurrentData == "line" or self.CurrentData =="marker":
-	  
-         self.line = content
-	 
+      #elif self.CurrentData == "line" or self.CurrentData =="marker":
+      elif self.CurrentData == "line":  
+       self.line = content
+       #pdb.set_trace()
+       self.page_tmp=self.page_tmp +" "+ self.line;
+       if(self.pag_cont!=""): 
+         self.line=self.pag_cont;
+         self.pag_cont=""
 	 self.special_characters(self.line);
 	 word=self.line.split(' ')
-	# pdb.set_trace()
+	 pdb.set_trace()
 	 for i in range(len(word)):
 	     self.valid=1;#self.stp_word_chk(word[i]);
 	     if (self.valid==1 and word[i]!=""):
 #		print word[i]
+		
 		self.count=self.count+1;
 	 	if word[i] in self.inv_index:
 		   if self.book_count in self.inv_index[word[i]]:
@@ -128,6 +177,11 @@ class DocHandler( xml.sax.ContentHandler ):
 			self.inv_index[word[i]]={};
 			self.inv_index[word[i]][self.book_count]={}
  			self.inv_index[word[i]]['total']=1;
+			self.inv_index[word[i]]['word_pair']={}
+			self.inv_index[word[i]]['word_pair']['powerful']=[0,0,0];
+			self.inv_index[word[i]]['word_pair']['strong']=[0,0,0];
+			self.inv_index[word[i]]['word_pair']['salt']=[0,0,0];
+			self.inv_index[word[i]]['word_pair']['butter']=[0,0,0];
 			self.inv_index[word[i]][self.book_count][self.CurrentPage]=1;
                         self.uniq_count=self.uniq_count+1;
 #	 self.count=self.count+len(word)
@@ -225,19 +279,20 @@ for i in range(50):
 	tokenBF=(len(u)-1);
 	tokenPF=(tokenPF_calc(u));
 	tokenTF=(inv_index[n[i]]['total'])
-	tokenP=tokenTF/len(n);
-	tokenPR=tokenP*(i+1);
+	tokenP=float(tokenTF)/len(n);
+	tokenPR=float(tokenP)*(i+1);
 	print "VNM " + str(i+1) + ' ' + tokenString + ' ' + str(tokenBF) + ' ' + str(tokenPF) + ' ' + str(tokenTF)+ ' ' + str(tokenP) + ' ' + str(tokenPR);
 	#for x in n: inv_index[x]
 new_list=['powerful','strong','butter','salt','washington','james', 'church'];
 for i in range(7):                                                                                                                                                                                                                                         
         u=Handler.inv_index[new_list[i]];                                                                                                                                                                                                                          
         tokenString=new_list[i];                                                                                                                                                                                                                                   
-        tokenBF=(len(u)-1);                                                                                                                                                                                                                                 
+        tokenBF=(len(u)-2);                                                                                                                                                                                                                                 
         tokenPF=(tokenPF_calc(u));                                                                                                                                                                                                                          
         tokenTF=(inv_index[new_list[i]]['total'])                                                                                                                                                                                                                  
-        tokenP=tokenTF/len(n);                                                                                                                                                                                                                              
-        tokenPR=tokenP*(i+1);                                                                                                                                                                                                                               
+        tokenP=float(tokenTF)/len(n);                                                                                                                                                                                                                           
+        tokenPR=float(tokenP)*(i+1); 
+	                                                                                                                                                                                                           
         print "VNM " + str(n.index(new_list[i])+1) + ' ' + tokenString + ' ' + str(tokenBF) + ' ' + str(tokenPF) + ' ' + str(tokenTF)+ ' ' + str(tokenP) + ' ' + str(tokenPR);
-pdb.set_trace()
-print Handler.inv_index['powerful']
+#pdb.set_trace()
+#print Handler.inv_index['powerful']
