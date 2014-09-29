fb15c1a91e15f039c50bd1a15ead30756069eec4
Venkatesh Narasimha Murthy <venk@whyalla.cs.umass.edu>
2014-09-29 03:55:21 -0400

    updated

--- P1/Parse_XML_working.py
+++ P1/Parse_XML_working.py
@@ -59,7 +59,7 @@ class DocHandler( xml.sax.ContentHandler ):
       #pdb.set_trace()
       #self.pag_cont=self.page_tmp;
       #self.page_tmp="";
-      if (tag == 'page'):
+      if (tag == 'page' or tag=='marker'):
 	self.pag_cont=self.page_tmp;                                                                                                                               
         self.page_tmp="";
 	if(self.pag_cont!=""):    
@@ -68,8 +68,16 @@ class DocHandler( xml.sax.ContentHandler ):
          self.pag_cont=""                                                                                                                                                                                                                                   
          self.special_characters(self.line);                                                                                                                                                                                                                
          word=self.line.split(' ')   
-	 special_words=['powerful', 'strong', 'butter', 'salt'];                                                                                                                                                                                                                       
-        # pdb.set_trace()                                                                                                                                                                                                                                   
+         #pdb.set_trace()
+	 special_words=['powerful', 'strong', 'butter', 'salt', 'washington', 'james', 'church'];                                                                                                                                                                                                                       
+        # pdb.set_trace()
+         Indicator=[0]* len(special_words);
+	 for i in range(len(special_words)):
+		if special_words[i] in  word:
+			Indicator[i]=1;
+
+		
+	                                                                                                                                                                                                                               
          for i in range(len(word)):                                                                                                                                                                                                                         
              self.valid=1;#self.stp_word_chk(word[i]);                                                                                                                                                                                                      
              if (self.valid==1 and word[i]!=""):                                                                                                                                                                                                            
@@ -92,10 +100,59 @@ class DocHandler( xml.sax.ContentHandler ):
                 else:                                                                                                                                                                                                                                       
                         self.inv_index[word[i]]={};                                                                                                                                                                                                         
                         self.inv_index[word[i]][self.book_count]={}                                                                                                                                                                                         
-                        self.inv_index[word[i]]['total']=1;                                                                                                                                                                                                 
+                        self.inv_index[word[i]]['total']=1;    
+			self.inv_index[word[i]]['word_pair']={}                                                                                                                                                                                             
+                        self.inv_index[word[i]]['word_pair']['powerful']=[0,0,0]; #pageword1 windowword1 adjacentword1
+                        self.inv_index[word[i]]['word_pair']['strong']=[0,0,0];                                                                                                                                                                             
+                        self.inv_index[word[i]]['word_pair']['salt']=[0,0,0];                                                                                                                                                                               
+                        self.inv_index[word[i]]['word_pair']['butter']=[0,0,0];    
+			self.inv_index[word[i]]['word_pair']['washington']=[0,0,0];                                                                                                                                                                         
+                        self.inv_index[word[i]]['word_pair']['james']=[0,0,0];                                                                                                                                                                              
+                        self.inv_index[word[i]]['word_pair']['church']=[0,0,0];                                                                                                                                                                                              
                         self.inv_index[word[i]][self.book_count][self.CurrentPage]=1;                                                                                                                                                                       
                         self.uniq_count=self.uniq_count+1; 
+	     self.stp_word_chk(word[i]);
+	     if(self.valid==1 and  word[i]!="" and word[i]!=''):
+		 
+		 #pdb.set_trace()
+		 for j in range(len(special_words)):
+			if (Indicator[j]==1 and (word[i]!=special_words[j])):
+			#	pdb.set_trace()
+				self.inv_index[word[i]]['word_pair'][special_words[j]][0]+=1;
+				if(word[i-1]==special_words[j]):
+					self.inv_index[word[i]]['word_pair'][special_words[j]][2]+=1;
+
+	         
+		 if((i%20==0 and i!=0) or i==(len(word)-1)) :
+			
+			
+		        if(i==(len(word)-1) and i>20):
+				remi=(i%20);
+				end_i=i;start_i=i-remi;
+		        elif(i%20==0 and i!=(len(word)-1)):
+				end_i=i;start_i=i-20;
+			else:
+				end_i=i+1;start_i=i-20;
+				if(start_i<0):
+					start_i=0;
+			
+		        
+			
+			#Ind2=[0]* len(special_words);
+			#pdb.set_trace()
+			sub_word=word[start_i:end_i];
+			#sub_word=word[i:i+20];
+         		for j in range(len(special_words)):
 	
+                		if special_words[j] in  sub_word:
+					#if(j==0):
+#						pdb.set_trace()
+                        		for k in range(len(sub_word)):
+						self.stp_word_chk(sub_word[k]);
+						if(sub_word[k]!=special_words[j] and 	self.valid==1 and sub_word[k]!=""):
+							self.inv_index[sub_word[k]]['word_pair'][special_words[j]][1]+=1;	
+			
+		
      #    print "Region:", self.region
 #      elif self.CurrentData == "section":	
  #        print "Section:", self.section
@@ -178,14 +235,19 @@ class DocHandler( xml.sax.ContentHandler ):
 			self.inv_index[word[i]][self.book_count]={}
  			self.inv_index[word[i]]['total']=1;
 			self.inv_index[word[i]]['word_pair']={}
-			self.inv_index[word[i]]['word_pair']['powerful']=[0,0,0];
+			self.inv_index[word[i]]['word_pair']['powerful']=[0,0,0]; #pageword1 windowword1 adjacentword1
 			self.inv_index[word[i]]['word_pair']['strong']=[0,0,0];
 			self.inv_index[word[i]]['word_pair']['salt']=[0,0,0];
 			self.inv_index[word[i]]['word_pair']['butter']=[0,0,0];
+			self.inv_index[word[i]]['word_pair']['washington']=[0,0,0];
+			self.inv_index[word[i]]['word_pair']['james']=[0,0,0];
+			self.inv_index[word[i]]['word_pair']['church']=[0,0,0];
 			self.inv_index[word[i]][self.book_count][self.CurrentPage]=1;
                         self.uniq_count=self.uniq_count+1;
 #	 self.count=self.count+len(word)
-
+		
+		
+	
 #         print self.Book 
 	 #print "Word Count:", self.counter
    def ignorableWhitespace(self, whitespace):
@@ -222,7 +284,7 @@ if ( __name__ == "__main__"):
    #count=0;
    
    #for root, dirs, files in os.walk("/home/venk/Documents/Information_Retrieval/sample_python/P1/data/"):
-   for root, dirs, files in os.walk("/home/venk/Documents/Information_Retrieval/books-tiny/"):
+   for root, dirs, files in os.walk("/home/venk/Documents/Information_Retrieval/books-large/"):
      for file in files:
 	if file.endswith(".xml"):
 		print "Processing... ",file
@@ -270,29 +332,49 @@ if ( __name__ == "__main__"):
 #print book_count
 #print Handler.inv_index
 n=sorted(inv_index, key=lambda x: (inv_index[x]['total']),reverse=True)
+n_powerful_pageword=sorted(inv_index, key=lambda x: (inv_index[x]['word_pair']['powerful'][0]),reverse=True)
+#print n_powerful_pageword
+n_powerful_windowword=sorted(inv_index, key=lambda x: (inv_index[x]['word_pair']['powerful'][1]),reverse=True)
+n_powerful_adjacentword=sorted(inv_index, key=lambda x: (inv_index[x]['word_pair']['powerful'][2]),reverse=True)
 print "VNM tiny N " + str(book_count) + ' ' + str(Handler.page_count)                                                                                                                                                                                    
 print "VNM tiny TO " + str(Handler.count)                                                                                                                                                                                                                
 print "VNM tiny TU " + str(Handler.uniq_count)
-for i in range(50):
+for i in range(10):
+	#pdb.set_trace()
 	u=Handler.inv_index[n[i]];
 	tokenString=n[i];
-	tokenBF=(len(u)-1);
+	tokenBF=(len(u)-2);
 	tokenPF=(tokenPF_calc(u));
 	tokenTF=(inv_index[n[i]]['total'])
-	tokenP=float(tokenTF)/len(n);
+	tokenP=float(tokenTF)/(Handler.count);
 	tokenPR=float(tokenP)*(i+1);
 	print "VNM " + str(i+1) + ' ' + tokenString + ' ' + str(tokenBF) + ' ' + str(tokenPF) + ' ' + str(tokenTF)+ ' ' + str(tokenP) + ' ' + str(tokenPR);
 	#for x in n: inv_index[x]
 new_list=['powerful','strong','butter','salt','washington','james', 'church'];
+# '''
 for i in range(7):                                                                                                                                                                                                                                         
         u=Handler.inv_index[new_list[i]];                                                                                                                                                                                                                          
         tokenString=new_list[i];                                                                                                                                                                                                                                   
         tokenBF=(len(u)-2);                                                                                                                                                                                                                                 
         tokenPF=(tokenPF_calc(u));                                                                                                                                                                                                                          
         tokenTF=(inv_index[new_list[i]]['total'])                                                                                                                                                                                                                  
-        tokenP=float(tokenTF)/len(n);                                                                                                                                                                                                                           
+        tokenP=float(tokenTF)/(Handler.count);                                                                                                                                                                                                                           
         tokenPR=float(tokenP)*(i+1); 
 	                                                                                                                                                                                                           
         print "VNM " + str(n.index(new_list[i])+1) + ' ' + tokenString + ' ' + str(tokenBF) + ' ' + str(tokenPF) + ' ' + str(tokenTF)+ ' ' + str(tokenP) + ' ' + str(tokenPR);
+#'''
+for j in range(7):
+ n_pageword=sorted(inv_index, key=lambda x: (inv_index[x]['word_pair'][new_list[j]][0]),reverse=True)                                                                                                                                                
+#print n_powerful_pageword                                                                                                                                                                                                                                  
+ n_windowword=sorted(inv_index, key=lambda x: (inv_index[x]['word_pair'][new_list[j]][1]),reverse=True)                                                                                                                                              
+ n_adjacentword=sorted(inv_index, key=lambda x: (inv_index[x]['word_pair'][new_list[j]][2]),reverse=True)
+ for i in range(5):
+
+	print "VNM" + ' ' +new_list[j] + ' ' +  n_pageword[i] + ' ' + n_windowword[i] + ' ' + n_adjacentword[i]
+
+#print n_powerful_windowword
+#n_powerful_pageword=sorted(inv_index, key=lambda x: (inv_index[x]['word_pair']['powerful'][0]),reverse=True)
+#print n_powerful_pageword
 #pdb.set_trace()
-#print Handler.inv_index['powerful']
+#print Handler.inv_index['battery']['word_pair']
+#print Handler.inv_index['one']['word_pair']
